9228bb7d04462d599185e24ed95f8065fed7a577
oceanbig093 <innervision41@mail.ru>
2024-11-07 18:59:58 +0300

    Update module9_7.py

--- module9_7.py
+++ module9_7.py
@@ -7,7 +7,7 @@ def is_prime(func):
             if result % i == 0:
                 return "Составное"
         print(result)
-        return "Простое" #, result тоже не то
+        return "Простое" 
     return wrapper
 
 @ is_prime
@@ -15,5 +15,5 @@ def sum_three(a, b, c):
     return a + b + c
 
 
-result = sum_three(2, 3, 6)  # Возвращает "Простое"
+result = sum_three(2, 3, 6) 
 print(result)
