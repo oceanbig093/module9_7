c251f4ed88fb85a8e4ad24f2aed831e3e07ce815
oceanbig093 <innervision41@mail.ru>
2024-11-07 18:58:51 +0300

    Add files via upload

---
+++ module9_7.py
@@ -0,0 +1,19 @@
+def is_prime(func):
+    def wrapper(a, b, c):
+        result = func(a, b, c)
+        if result < 2:
+            return "Составное"  # 0 и 1 не являются простыми числами
+        for i in range(2, int(result ** 0.5) + 1):
+            if result % i == 0:
+                return "Составное"
+        print(result)
+        return "Простое" #, result тоже не то
+    return wrapper
+
+@ is_prime
+def sum_three(a, b, c):
+    return a + b + c
+
+
+result = sum_three(2, 3, 6)  # Возвращает "Простое"
+print(result)
